6f516176c02711fb861e881d55a1ccb67f0c4ba0
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-10-08 19:17:57 +0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-import Navbar from './components/Navbar/index'
-import Home from './pages/main/home'
+import Header from './components/Header/Header.jsx'
+// import Navbar from './components/Navbar/navbar.jsx'
+import Home from './pages/main/home/home.jsx'
 import Detail from './pages/main/detail'
 import About from './pages/main/about'
 
-function App() {
+function App () {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Header />
+      {/* <Navbar /> */}
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/countries/:contries" component={Detail} />
-        <Route path="/about" component={About} />
+        <Route path='/' exact component={Home} />
+        <Route path='/countries/:contries' component={Detail} />
+        <Route path='/about' component={About} />
       </Switch>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Header.css'
+export default class Header extends Component {
+  render () {
+    return (
+      <header className='container-flud bg-info d-flex justify-content-center align-items-center'>
+        <h2>Data Statistic Covid 19</h2>
+      </header>
+    )
+  }
+}
