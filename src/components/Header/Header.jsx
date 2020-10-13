b7a21d296f435f320f25327d8556abd349b0a428
Ervin-Nurhediyanto <cafeinai311@gmail.com>
2020-10-14 00:55:31 +0700

    Covid-19 Image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import CovidIMG from '../../assets/Covid-19.png'
 import './Header.css'
 export default class Header extends Component {
   render () {
     return (
       <header className='container-flud bg-info d-flex justify-content-center align-items-center'>
-        <h2>Data Covid 19</h2>
+        <div className='header-image'><img src={`${CovidIMG}`} alt='' /></div>
+        <div className='ml-2 mr-2'><h2>Data Covid 19</h2></div>
+        <div className='header-image'><img src={`${CovidIMG}`} alt='' /></div>
       </header>
     )
   }
